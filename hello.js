2bed77be76ae4887a88960d270f8374549829d81
Yelaman Uisunbayev <yelaman.ui@gmail.com>
2021-12-28 16:15:51 +0600

    updated package.json added procfile

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000 
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
@@ -21,7 +22,7 @@ http.createServer(function (req, res) {
       return res.end();
     }
   })
-}).listen(8080);
+}).listen(PORT);
 
 
 console.log('Server listening on port 8080...')
